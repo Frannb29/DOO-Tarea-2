7b0db061afce1f397c94e6f2ad6b1b999be5caee
Francisca Hidalgo <franhidalgopezo@gmail.com>
2026-05-20 23:21:31 -0400

    feat: implementa clases Asistencia y Retraso

--- src/main/java/modelo/Asistencia.java
+++ src/main/java/modelo/Asistencia.java
@@ -1,5 +1,18 @@
 package modelo;
 
 public class Asistencia {
-
+    private Invitable asistente;
+    public Asistencia (Invitable asistente){
+        this.asistente = asistente;
+    }
+    public Invitable getAsistente(){
+        return asistente;
+    }
+    public void setAsistente(Invitable asistente){
+        this.asistente = asistente;
+    }
+    @Override
+    public String toString() {
+        return "Asistente: " + asistente.toString();
+    }
 }
--- src/main/java/modelo/Retraso.java
+++ src/main/java/modelo/Retraso.java
@@ -1,4 +1,19 @@
 package modelo;
-
+import java.time.Instant;
 public class Retraso extends Asistencia {
+    private Instant horaRetraso;
+    public Retraso(Invitable invitable, Instant horaRetraso){
+        super(invitable);
+        this.horaRetraso = horaRetraso;
+    }
+    public Instant getHoraRetraso(){
+        return horaRetraso;
+    }
+    public void setHora(Instant horaRetraso){
+        this.horaRetraso = horaRetraso;
+    }
+    @Override
+    public String toString() {
+        return super.toString() + " | Hora de llegada: " + horaRetraso;
+    }
 }
